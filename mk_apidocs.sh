64fed7621f3e04d426f0d21ba167417cea2f9652
schryer <schryer@539f11d8-17cb-11df-bcdc-a7d16bff891f>
2010-06-29 10:50:56 +0000

    Changed documentation generation scripts.  Moved index.html message generation into the clean method of the Sphinx Makefile.  Removed the cens HTML_LOCATION in the Sphinx Makefile since only one is valid and I think this sould be copied or linked to cens seperatly.

--- mk_apidocs.sh
+++ mk_apidocs.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-rm -rf doc/_build/ doc/source/generated /net/cens/home/www/sysbio/download/software/iocbio/*
-echo `date -u` ": documentation is being updated... try again in few minutes." >  /net/cens/home/www/sysbio/download/software/iocbio/index.html
 PYTHONPATH=`pwd`:$PYTHONPATH
-cd doc && make html || exit 1
+
+cd doc && make clean && make html || exit 1
 cd -
 exit 0
-
-#rm -rf apidocs/.buildinfo doc/_build doc/source/generated # full rebuild
 
