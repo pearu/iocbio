560688d55133cdbeb9dad00f04a760267dc5da10
pearu.peterson <pearu.peterson@539f11d8-17cb-11df-bcdc-a7d16bff891f>
2010-10-12 08:25:20 +0000

    Created advanced interface to dg_high_pass_filter: fftw plans are created once.

--- iocbio/ops/src/discrete_gauss.c
+++ iocbio/ops/src/discrete_gauss.c
@@ -1,7 +1,11 @@
 
 #include <stdlib.h>
 #include <math.h>
+#include <assert.h>
+#include <string.h>
+
 #include "discrete_gauss.h"
+
 
 int dg_convolve(double *seq, int n, double t)
 {
@@ -28,46 +32,123 @@ int dg_convolve(double *seq, int n, double t)
   return 0;
 }
 
-int dg_high_pass_filter(double *seq, int n, int rows, double t)
+int dg_DGR2HCConfig_init(DGR2HCConfig *config, int rank, int dims[3], int howmany)
 {
-  int k, m;
-  double c;
-  double *fseq = (double*)malloc((n*rows)*sizeof(double));
-  int n_dims[] = {n};
-  fftw_r2r_kind kinds[] = {FFTW_R2HC};
-  fftw_r2r_kind ikinds[] = {FFTW_HC2R};
-  fftw_plan plan = fftw_plan_many_r2r(1, // rank 
-				      n_dims, // n
-				      rows, // howmany
-				      seq, // in
-				      NULL, // inembed
-				      1, // istride
-				      n, // idist
-				      fseq, NULL, 1, n,
-				      kinds, 
-				      FFTW_ESTIMATE);
-  fftw_plan iplan = fftw_plan_many_r2r(1, n_dims, rows, 
-				       fseq, NULL, 1, n,
-				       seq, NULL, 1, n,
-				       ikinds, 
-				       FFTW_ESTIMATE);
-  fftw_execute(plan);
-  for (m=0; m<rows; ++m)
+  int i, sz;
+  fftw_r2r_kind *r2hckinds = NULL;
+  fftw_r2r_kind *hc2rkinds = NULL;
+  config->rank = rank;
+  r2hckinds = (fftw_r2r_kind*)malloc(rank*sizeof(fftw_r2r_kind));
+  hc2rkinds = (fftw_r2r_kind*)malloc(rank*sizeof(fftw_r2r_kind));
+  sz = 1;
+  for (i=0; i<rank; ++i)
     {
-      /* real part: */
-      (fseq+m*n)[0] = 0.0; /* DC component */
+      sz *= dims[i];
+      config->dims[i] = dims[i];
+      r2hckinds[i] = FFTW_R2HC;
+      hc2rkinds[i] = FFTW_HC2R;
+    }
+  config->howmany = howmany;
+  config->sz = sz;
+  config->rdata = (double*)malloc((sz*howmany)*sizeof(double));
+  config->hcdata = (double*)malloc((sz*howmany)*sizeof(double));
+  config->r2hc_plan = fftw_plan_many_r2r(rank, // rank 
+					 config->dims, // n
+					 howmany, // howmany
+					 config->rdata, // in
+					 NULL, // inembed
+					 1, // istride
+					 sz, // idist
+					 config->hcdata, NULL, 1, sz,
+					 r2hckinds,
+					 FFTW_ESTIMATE);
+  config->hc2r_plan = fftw_plan_many_r2r(rank, // rank 
+					 config->dims, // n
+					 howmany, // howmany
+					 config->hcdata, // in
+					 NULL, // inembed
+					 1, // istride
+					 sz, // idist
+					 config->rdata, NULL, 1, sz,
+					 hc2rkinds,
+					 FFTW_ESTIMATE);
+  free(r2hckinds);
+  free(hc2rkinds);
+  return 0;
+}
+
+int dg_DGR2HCConfig_clean(DGR2HCConfig *config)
+{
+  fftw_destroy_plan(config->r2hc_plan);
+  fftw_destroy_plan(config->hc2r_plan);
+  if (config->rdata != NULL)
+    free(config->rdata);
+  if (config->hcdata != NULL)
+    free(config->hcdata);
+  if (config->kernel_real != NULL)
+    free(config->kernel_real);
+  if (config->kernel_imag != NULL)
+    free(config->kernel_imag);
+  config->rdata = NULL;
+  config->hcdata = NULL;
+  config->kernel_real = NULL;
+  config->kernel_imag = NULL;
+  config->sz = config->rank = 0;
+  return 0;
+}
+
+int dg_DGR2HCConfig_apply_real_filter(DGR2HCConfig *config)
+{
+  int m, k;
+  int n = config->dims[0];
+  assert(config->rank==1);
+  for (m=0; m<config->howmany; ++m)
+    {
+      (config->hcdata+m*n)[0] *= config->kernel_real[0];
       for (k = 1; k < (n+1)/2; ++k)  /* (k < N/2 rounded up) */
 	{
-	  c = (1.0-exp((cos((2.0*M_PI*k)/n)-1.0)*t))/n;
-	  (fseq+m*n)[k] *= c; // real part
-	  (fseq+m*n)[n-k] *= c; // imaginary part
+	  (config->hcdata+m*n)[k] *= config->kernel_real[k]; // real part
+	  (config->hcdata+m*n)[n-k] *= config->kernel_real[k]; // imaginary part
 	}
       if (n % 2 == 0) /* N is even */
-	(fseq+m*n)[n/2] *= (1.0-exp((cos((2.0*M_PI*(n/2))/n)-1.0)*t))/n;  /* Nyquist freq. */
+	(config->hcdata+m*n)[n/2] *= config->kernel_real[n/2];  /* Nyquist freq. */
     }
-  fftw_execute(iplan);
-  fftw_destroy_plan(plan);
-  fftw_destroy_plan(iplan);
-  free(fseq);
+  return 0;
+}
+
+int dg_high_pass_filter_init(DGR2HCConfig *config, int rank, int dims[3], int howmany, double scale_parameter)
+{
+  int k;
+  dg_DGR2HCConfig_init(config, rank, dims, howmany);
+  config->kernel_real = (double*)malloc(config->sz*sizeof(double));
+  config->kernel_imag = NULL;
+  assert(rank==1); // todo: generalize to rank 2 and 3
+  for (k=0; k<(dims[0]+1)/2; ++k)
+    config->kernel_real[k] = (1.0-exp((cos((2.0*M_PI*k)/dims[0])-1.0)*scale_parameter))/dims[0];
+  return 0;
+}
+
+int dg_high_pass_filter_clean(DGR2HCConfig *config)
+{
+  return dg_DGR2HCConfig_clean(config);
+}
+
+int dg_high_pass_filter_apply(DGR2HCConfig *config, double *seq)
+{
+  memcpy(config->rdata, seq, config->sz*config->howmany*sizeof(double));
+  fftw_execute(config->r2hc_plan);
+  dg_DGR2HCConfig_apply_real_filter(config);
+  fftw_execute(config->hc2r_plan);
+  memcpy(seq, config->rdata, config->sz*config->howmany*sizeof(double));
+  return 0;
+}
+
+int dg_high_pass_filter(double *seq, int n, int rows, double t)
+{
+  DGR2HCConfig config;
+  int dims[3] = {n,1,1};
+  dg_high_pass_filter_init(&config, 1, dims, rows, t);
+  dg_high_pass_filter_apply(&config, seq);
+  dg_high_pass_filter_clean(&config);
   return 0;
 }
--- iocbio/ops/src/discrete_gauss.h
+++ iocbio/ops/src/discrete_gauss.h
@@ -1,6 +1,29 @@
 #ifndef DISCRETE_GAUSS_H_INCLUDE
 #define DISCRETE_GAUSS_H_INCLUDE
 #include "fftw3.h"
+
+typedef struct 
+{
+  int rank;
+  int dims[3];
+  int howmany;
+  int sz;              /* prod(dims) */
+  double *rdata;
+  double *hcdata;      /* fft of rdata */
+  double *kernel_real; /* filter kernel: real part */
+  double *kernel_imag; /* filter kernel: imaginary part */
+  fftw_plan r2hc_plan; 
+  fftw_plan hc2r_plan; 
+} DGR2HCConfig;
+
+
+// user interface:
 extern int dg_convolve(double *seq, int n, double t);
 extern int dg_high_pass_filter(double *seq, int n, int rows, double t);
+
+// user interface with fftw plan caching:
+extern int dg_high_pass_filter_init(DGR2HCConfig *config, int rank, int dims[3], int howmany, double scale_parameter);
+extern int dg_high_pass_filter_clean(DGR2HCConfig *config);
+extern int dg_high_pass_filter_apply(DGR2HCConfig *config, double *seq);
+
 #endif
