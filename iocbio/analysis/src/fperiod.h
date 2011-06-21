cc17b83721fd462fd6ed901f0c2419819a7155a1
pearu.peterson <pearu.peterson@539f11d8-17cb-11df-bcdc-a7d16bff891f>
2011-06-21 09:45:50 +0000

    Fix fperiod header files to be C++ safe.

--- iocbio/analysis/src/fperiod.h
+++ iocbio/analysis/src/fperiod.h
@@ -8,6 +8,9 @@
 #ifndef FPERIOD_H
 #define FPERIOD_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 extern double fperiod_compute_period(double* f, int n, int m, double structure_size, double exp, int method);
 extern double fperiod_compute_period_cache(double* f, int n, int m, double structure_size, double exp, int method, double *r);
 extern double fperiod_find_acf_maximum(double* f, int n, int m, int lbound, int ubound);
@@ -18,5 +21,9 @@ extern double fperiod_acf_d2(double y, double* f, int n, int m);
 extern void fperiod_subtract_average1(double* f, int n, int fstride, int smoothness, double* r, int rstride);
 extern void fperiod_subtract_average(double* f, int n, int m, int structure_size, double* r);
 extern void fperiod_subtract_average_2d(double* f, int n, int m, int smoothness, double* r);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- iocbio/analysis/src/imageinterp.h
+++ iocbio/analysis/src/imageinterp.h
@@ -8,9 +8,16 @@
 #ifndef IMAGEINTERP_H
 #define IMAGEINTERP_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 extern void imageinterp_get_roi(int image_width, int image_height, double *image,
 				int i0, int j0, int i1, int j1, double width,
 				int roi_width, int roi_height, double *roi
 				);
+#ifdef __cplusplus
+}
+#endif
 
 #endif
