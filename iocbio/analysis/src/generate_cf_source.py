74d6e81ceb659bffd2f8becf7357016ca4b3b696
pearu.peterson <pearu.peterson@539f11d8-17cb-11df-bcdc-a7d16bff891f>
2011-09-20 22:09:09 +0000

    WIP

--- iocbio/analysis/src/generate_cf_source.py
+++ iocbio/analysis/src/generate_cf_source.py
@@ -4,7 +4,7 @@ Generate code for evaluating correlation functions.
 """
 
 import sys
-from sympycore import Symbol, Calculus, PolynomialRing
+from sympycore import Symbol, Calculus, PolynomialRing, Expr, heads
 
 class Indexed:
     def __init__(self, name, index):
@@ -54,36 +54,64 @@ class Generator:
         self.namespace = dict(s = R('s'), r=R('r'), i=Symbol('i'),
                               j = Symbol('j'), N=Symbol('N'),
                               f = IndexedGenerator(R, 'f'),
-                              pwf = pwf)
+                              pwf = pwf, R=R)
 
-    def convolution(self):
+    def integrate(self, integrand='f(x)*f(x+y)'):
+        """ Integrate integrand of piecewise polynomial functions
+        within bounds [0, N-1-y] where N is the number of functions
+        nodal values, y=j+r, 0<=r<1, j is integer in [0, N-1].
+
+        Parameters
+        ----------
+        integrand : str
+          Specify integrand as string expression. The expression can contain
+          substrings `f(x)`, `f(x+y)`, `x` that are treated specially.
+          The expression may not contain substring `i`.
+
+        Returns
+        -------
+        integral_i, integral_r : PolynomialRing['s','r']
+          Integral is sum(integral_i, i=0..N-3-j) + integral_r
+          where integral_(i|r) is a polynomial of r.
+        """
         for k,v in self.namespace.iteritems():
             exec k+' = v'
 
-        # int(f(x)*f(x+y), x=0..N-1)
-        integrand1 = pwf(f,i,s)*pwf(f,i+j,s+r)+pwf(f,N-2-j, s)*pwf(f,N-2,s+r)
-        integrand2 = pwf(f,i,s)*pwf(f,i+j+1,s-(1-r))
+        integrand1 = eval(integrand.replace('f(x)','pwf(f,i,s)').replace('f(x+y)','pwf(f,i+j,s+r)').replace('x','(R.Number(i)+s)'))
+        integrand2 = eval(integrand.replace('f(x)','pwf(f,N-2-j, s)').replace('f(x+y)','pwf(f,N-2,s+r)').replace('x','(R.Number(N-2-j)+s)'))
+        integrand3 = eval(integrand.replace('f(x)','pwf(f,i,s)').replace('f(x+y)','pwf(f,i+j+1,s+r-1)').replace('x','(R.Number(i)+s)'))
+
         integral1 = integrand1.variable_integrate(s, 0, 1-r)
-        integral2 = integrand2.variable_integrate(s, 1-r, 1)
-        integral_fx_fxy = (integral1 + integral2).expand() # sum(intergal_fx_fxy, i=0..N-2-j)
-    
-        return integral_fx_fxy
+        integral2 = integrand2.variable_integrate(s, 0, 1-r)
+        integral3 = integrand3.variable_integrate(s, 1-r, 1)
+        integral_i1 = (integral1 + integral3).expand()
+        integral_r = (integral2).expand()
+
+        integral_i = R({})
+        for e,c in integral_i1.data.iteritems():
+            if isinstance(c, Expr):
+                c = c.head.to_ADD(type(c), c.data, c)
+                if c.head is heads.ADD:
+                    data = c.data
+                else:
+                    data = [c]
+            else:
+                data = [c]
+            for c in data:
+                if 'i' in str(c):
+                    integral_i += R({e:c})
+                else:
+                    integral_r += R({e:c*(N-2-j)})
+
+        return integral_i, integral_r
 
     def show_convolution(self):
-        poly = self.convolution()
-        for k in sorted(poly.data):
-            expr = poly.data[k]
-            expr = expr.head.to_ADD(type(expr), expr.data, expr)
-            assert str(expr.head)=='ADD',`expr.head`
-            loop_terms = []
-            nonloop_terms = []
-            for term in expr.data:
-                if 'i' in str(term):
-                    loop_terms.append(term)
-                else:
-                    nonloop_terms.append(term)
+        poly_i, poly_r = self.integrate('f(x)*f(x+y)')
+        for k in sorted(set(poly_i.data.keys() + poly_r.data.keys())):
+            expr_i = poly_i.data.get(k, 0)
+            expr_r = poly_r.data.get(k, 0)
                 
-            print '%s: %s + {%s}' % (self.ring({k:1}), self.ring.ring.Add(*loop_terms), self.ring.ring.Add(*nonloop_terms))
+            print '%s: sum(%s,i=0..N-3-j) + {%s}' % (self.ring({k:1}), expr_i, expr_r)
 
 g = Generator('linear')
 g.show_convolution()
