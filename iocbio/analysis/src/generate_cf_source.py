a798f3f8ad34820dec2d89dd4fe93c12d7c6c5e7
pearu.peterson <pearu.peterson@539f11d8-17cb-11df-bcdc-a7d16bff891f>
2011-09-21 22:32:13 +0000

    WIP

--- iocbio/analysis/src/generate_cf_source.py
+++ iocbio/analysis/src/generate_cf_source.py
@@ -2,8 +2,12 @@
 Generate code for evaluating correlation functions.
 
 """
+# Author: Pearu Peterson
+# Created: September 2011
 
+import re
 import sys
+
 from sympycore import Symbol, Calculus, PolynomialRing, Expr, heads
 
 class Indexed:
@@ -26,6 +30,7 @@ class IndexedGenerator:
         self.ring = ring
         self.name = name
     def __getitem__(self, index):
+        #return self.ring.Number(index)
         return self.ring.Number(Symbol(Indexed(self.name, index)))
 
 class Generator:
@@ -52,11 +57,12 @@ class Generator:
         self.pwf = pwf
         self.ring = R = PolynomialRing[('s','r')]
         self.namespace = dict(s = R('s'), r=R('r'), i=Symbol('i'),
-                              j = Symbol('j'), N=Symbol('N'),
+                              o=Symbol('o'),
+                              j = Symbol('j'), N=Symbol('n'),
                               f = IndexedGenerator(R, 'f'),
                               pwf = pwf, R=R)
 
-    def integrate(self, integrand='f(x)*f(x+y)'):
+    def integrate(self, integrand='f(x)*f(x+y)', extension='cutoff'):
         """ Integrate integrand of piecewise polynomial functions
         within bounds [0, N-1-y] where N is the number of functions
         nodal values, y=j+r, 0<=r<1, j is integer in [0, N-1].
@@ -64,28 +70,37 @@ class Generator:
         Parameters
         ----------
         integrand : str
-          Specify integrand as string expression. The expression can contain
-          substrings `f(x)`, `f(x+y)`, `x` that are treated specially.
-          The expression may not contain substring `i`.
+          Specify integrand as string expression. The expression can
+          contain substrings `f(x)`, `f(x+y)`, `x` that are treated
+          specially.  The expression may not contain substrings `i`,
+          `s` and `r`.
+          
+        extension : {'cutoff', 'periodic'}
+          Specify f(x) extension outside its support interval. For
+          cutoff extension ``f(x)=0`` and for periodic
+          ``f(x)=f(x-(N-1))``, when ``x > N-1``.
 
         Returns
         -------
         integral_i, integral_r : PolynomialRing['s','r']
-          Integral is sum(integral_i, i=0..N-3-j) + integral_r
+          Integral is ``sum(integral_i, i=0..N-3-j) + integral_r``
           where integral_(i|r) is a polynomial of r.
         """
         for k,v in self.namespace.iteritems():
             exec k+' = v'
 
-        integrand1 = eval(integrand.replace('f(x)','pwf(f,i,s)').replace('f(x+y)','pwf(f,i+j,s+r)').replace('x','(R.Number(i)+s)'))
-        integrand2 = eval(integrand.replace('f(x)','pwf(f,N-2-j, s)').replace('f(x+y)','pwf(f,N-2,s+r)').replace('x','(R.Number(N-2-j)+s)'))
-        integrand3 = eval(integrand.replace('f(x)','pwf(f,i,s)').replace('f(x+y)','pwf(f,i+j+1,s+r-1)').replace('x','(R.Number(i)+s)'))
+        if extension=='cutoff':
+            integrand1 = eval(integrand.replace('f(x)','pwf(f,o+i,s)').replace('f(x+y)','pwf(f,o+i+j,s+r)').replace('x','(R.Number(i)+s)').replace('f(0)','f[o]'))
+            integrand2 = eval(integrand.replace('f(x)','pwf(f,o+N-2-j, s)').replace('f(x+y)','pwf(f,o+N-2,s+r)').replace('x','(R.Number(N-2-j)+s)').replace('f(0)','f[o]'))
+            integrand3 = eval(integrand.replace('f(x)','pwf(f,o+i,s)').replace('f(x+y)','pwf(f,o+i+j+1,s+r-1)').replace('x','(R.Number(i)+s)').replace('f(0)','f[o]'))
 
-        integral1 = integrand1.variable_integrate(s, 0, 1-r)
-        integral2 = integrand2.variable_integrate(s, 0, 1-r)
-        integral3 = integrand3.variable_integrate(s, 1-r, 1)
-        integral_i1 = (integral1 + integral3).expand()
-        integral_r = (integral2).expand()
+            integral1 = integrand1.variable_integrate(s, 0, 1-r)
+            integral2 = integrand2.variable_integrate(s, 0, 1-r)
+            integral3 = integrand3.variable_integrate(s, 1-r, 1)
+            integral_i1 = (integral1 + integral3).expand()
+            integral_r = (integral2).expand()
+        elif extension=='periodic' or 1:
+            raise NotImplementedError(`extension`)
 
         integral_i = R({})
         for e,c in integral_i1.data.iteritems():
@@ -105,16 +120,68 @@ class Generator:
 
         return integral_i, integral_r
 
-    def show_convolution(self):
-        poly_i, poly_r = self.integrate('f(x)*f(x+y)')
+    def show_convolution(self, integrand='f(x)*f(x+y)'):
+        poly_i, poly_r = self.integrate(integrand)
+
         for k in sorted(set(poly_i.data.keys() + poly_r.data.keys())):
             expr_i = poly_i.data.get(k, 0)
             expr_r = poly_r.data.get(k, 0)
                 
             print '%s: sum(%s,i=0..N-3-j) + {%s}' % (self.ring({k:1}), expr_i, expr_r)
 
+    def generate_source(self,
+                        name = 'mcf1',
+                        integrand = '(f(x)-f(0))*(2*f(x+y)-f(x)-f(0))',
+                        extension='cutoff'):
+        #self.show_convolution(integrand)
+        poly_i, poly_r = self.integrate(integrand)
+        exps = sorted(set(poly_i.data.keys() + poly_r.data.keys()))
+
+        decl_coeffs = ', '.join('double* a%s' % (i) for i in range(len(exps)))
+        init_coeffs = '\n  '.join('double b%s = 0.0;' % (i) for i in range(len(exps)))
+        set_coeffs = '\n  '.join('*a%s = b%s;' % (i, i) for i in range(len(exps)))
+
+        cf_source_template = '''
+%(cf_proto)s
+{
+  int p, i, q;
+  int k = n - 2 - j;
+  %(init_coeffs)s
+  if (k>=0)
+  {
+    for(p=0, o=0; p<m; ++p, o+=n)
+    {
+      for (i=0; i<k; ++i)
+      {
+        %(update_loop_coeffs)s
+      }
+      %(update_nonloop_coeffs)s
+    }
+  }
+  %(set_coeffs)s
+}
+        '''
+
+        for order in range(4):
+            poly_i_diff = poly_i.variable_diff(self.namespace['r'], order)
+            poly_r_diff = poly_r.variable_diff(self.namespace['r'], order)
+            diff_exps = sorted(set(poly_i_diff.data.keys() + poly_r_diff.data.keys()))
+
+            update_loop_coeffs = '\n        '.join('b%s += %s;' % (e[0], poly_i_diff.data.get(e, 0)) for e in diff_exps)
+            update_nonloop_coeffs = '\n      '.join('b%s += %s;' % (e[0], poly_r_diff.data.get(e, 0)) for e in diff_exps)
+            cf_proto = 'void cf_%(name)s_compute_coeffs_diff%(order)s(int j, double *f, int n, int m, %(decl_coeffs)s)' % (locals())
+
+            cf_source = cf_source_template % (locals())
+            cf_source = re.sub(r'(\(f\[(?P<index>[^\]]+)\]\)[*]{2,2}2)', r'(f[\g<index>]*f[\g<index>])', cf_source)
+            cf_source = re.sub(r'(\(f\[(?P<index>[^\]]+)\]\)[*]{2,2}(?P<exp>\d+))', r'pow(f[\g<index>], \g<exp>)', cf_source)
+            cf_source = re.sub(r'(?P<numer>\d+)[/](?P<denom>\d+)', r'\g<numer>.0/\g<denom>.0', cf_source)
+            yield cf_proto, cf_source
+
 g = Generator('linear')
-g.show_convolution()
+for proto, source in g.generate_source('mcf1_pw1'):
+    print source
+    pass
+
 
 
 
