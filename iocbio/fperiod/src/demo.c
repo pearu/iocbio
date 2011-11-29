44c51b0c73c63c1841776cee824cb5d4e6473e0a
pearu.peterson <pearu.peterson@539f11d8-17cb-11df-bcdc-a7d16bff891f>
2011-11-29 12:25:40 +0000

    Added period argument to fperiod demo.c

--- iocbio/fperiod/src/demo.c
+++ iocbio/fperiod/src/demo.c
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[])
 {
 
-  int n = 50; /* default length of a signal (one row) */
+  int n = 30; /* default length of a signal (one row) */
   int m = 1; /* number of signal rows */
   double *f = NULL; /* holds signal data */
   double period = 4.3; /* default value of expected period */
@@ -22,17 +22,26 @@ int main(int argc, char *argv[])
   switch(argc)
     {
     case 1: break;
-    case 2: n = atoi(argv[1]); break;
+    case 2: 
+      n = atoi(argv[1]); 
+      break;
+    case 3: 
+      n = atoi(argv[1]); 
+      period = atof(argv[2]); 
+      break;
+    default:
+      printf("Unexpected number of arguments: %d\nUsage: %s [<length of a signal> [<period of a signal>]]", argc, argv[0]);
     }
 
   printf("Signal f[i]=sin(2*pi*i/%f), i=0,1,..,%d\n", period, n-1);
-  printf("Nof repetitive patterns in signal=%f\n", n/period);
+  printf("Nof repetitive patterns in the signal=%f\n", n/period);
 
   f = (double*)malloc(sizeof(double)*n*m); 
   for (i=0;i<n;++i) /* initialize signal */
     {
       f[i] = sin(2.0*M_PI/period*i);
-      printf("f[%d] = %f\n", i, f[i]);
+      if (n<20)
+	printf("f[%d] = %f\n", i, f[i]);
     }
   /* Estimate fundamental period of the signal */
   fperiod = iocbio_fperiod(f, n, m, initial_period, detrend, method);
