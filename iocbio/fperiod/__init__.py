058e20f929bb643208af6a4c2f56787bf32acad0
pearu.peterson <pearu.peterson@539f11d8-17cb-11df-bcdc-a7d16bff891f>
2012-03-06 09:06:10 +0000

    Impl fperiod_acf_fft and fperiod_fft. Reviewed imageinterp_get_roi C function, fixed pixel size bugs.

--- iocbio/fperiod/__init__.py
+++ iocbio/fperiod/__init__.py
@@ -14,8 +14,144 @@ The :mod:`iocbio.fperiod` provides the following tools:
   trend_spline
   ipwf
 """
+from __future__ import division
 
 __autodoc__ = ['fperiod', 'detrend']
 
 from .fperiod_ext import fperiod, fperiod_cached, detrend, objective, trend_spline
 from . import ipwf
+
+def fperiod_acf_fft(image, detrend=True, quad_approx=True, zero_padding=2):
+    """ Estimate fundamental period of an image using the maximum point
+    of average ACF of image lines computed via FFT. 
+
+    WARNING: This function is implemented to demonstrate various flaws
+    of used method. In practical application usage of fperiod function
+    is highly recommended.
+
+    Parameters
+    ----------
+    image : ndarray
+      Specify image array. The periodic pattern is assumed to be
+      aligned in rows of the image.
+    detrend: bool
+      When True then detrend the image prior spectrum computation.
+    quad_approx: bool
+      When True then use quadratic approximation of the spectrum peak.
+    zero_padding : int
+      Positive integer used for padding Fourier transform of ACF with
+      zeros. For periodic images use zero_padding=1. For finite images
+      use zero_padding=2.
+
+    Returns
+    -------
+    fperiod : float
+      Estimated fundamental period of an image.
+    """
+    from numpy import fft, zeros, where, sqrt
+    if detrend:
+        from .fperiod_ext import detrend
+        image = detrend(image)
+    N = image.shape[-1]
+    fimage = fft.fft(image, axis=-1)
+    mfimage = abs(fimage*fimage.conjugate ())
+    if len (image.shape)>1:
+        mfimage = mfimage.mean(axis=0)
+    if zero_padding>1:
+        padded = zeros(N*zero_padding)
+        padded[:N//2+1] = mfimage[:N//2+1]
+        padded[-N//2:] = mfimage[-N//2:]
+        mfimage = padded
+    acf = fft.ifft(mfimage).real[:N//2]
+
+    imx = 0
+    for i in range (1,len (acf)-1):
+        if acf[i-1]<=acf[i] and acf[i]>=acf[i+1]:
+            imx = i
+            break
+
+    if quad_approx:
+        denom = (acf[imx-1]+acf[imx+1]-2*acf[imx])
+        if denom:
+            di = 0.5*(acf[imx-1] - acf[imx+1])/denom
+            assert abs(di)<=1,`imx,di`
+            return (imx+di)*(1.0/zero_padding)
+    return imx*(1.0/zero_padding)
+
+def fperiod_fft(image, detrend=True, quad_approx=True):
+    """ Estimate fundamental period of an image using the maximum point
+    of the Fourier spectrum of the image.
+
+    WARNING: This function is implemented to demonstrate various flaws
+    of used method. In practical application usage of fperiod function
+    is highly recommended.
+
+    Parameters
+    ----------
+    image : ndarray
+      Specify image array. The periodic pattern is assumed to be
+      aligned in rows of the image.
+    detrend: bool
+      When True then detrend the image prior spectrum computation.
+    quad_approx: bool
+      When True then use quadratic approximation of the spectrum peak.
+
+    Returns
+    -------
+    fperiod : float
+      Estimated fundamental period of an image.
+    """
+    from numpy import fft, zeros, where
+    if detrend:
+        from .fperiod_ext import detrend
+        image = detrend(image)
+
+    N = image.shape[-1]
+    fimage = fft.fft(image, axis=-1)
+
+    mfimage = abs(fimage*fimage.conjugate ())
+    if len (image.shape)>1:
+        mfimage = mfimage.mean (axis=0)
+    power_spectrum = zeros(N//2+1)
+    power_spectrum[0] = mfimage[0]
+    power_spectrum[N//2] = mfimage[N//2]
+    power_spectrum[1:N//2] = mfimage[1:N//2] + mfimage[::-1][:N//2-1] # NumRecip
+
+    imx = where(power_spectrum==power_spectrum.max())[0][0]    
+
+    if imx==0:
+        return None
+        from matplotlib import pyplot as plt
+        plt.subplot(211)
+        if len (image.shape)>1:
+            for line in image:
+                plt.plot(line)
+        else:
+            plt.plot(image)
+        plt.subplot(212)
+        plt.plot(power_spectrum)
+        plt.show ()
+        return None
+
+    if quad_approx:
+        if imx<N//2:
+            #print N, N//2+1, imx
+            numer = 0.5*(power_spectrum[imx-1] - power_spectrum[imx+1])
+            denom = (power_spectrum[imx-1]+power_spectrum[imx+1]-2*power_spectrum[imx])
+            if denom and abs (numer) <= abs (denom):
+                return N/(imx + numer/denom)
+            elif 1:
+                pass
+            else:
+                from matplotlib import pyplot as plt
+                plt.subplot(211)
+                if len (image.shape)>1:
+                    for line in image:
+                        plt.plot(line)
+                else:
+                    plt.plot(image)
+                plt.subplot(212)
+                plt.plot(power_spectrum)
+                plt.show ()
+
+    return N/imx
--- iocbio/fperiod/src/imageinterp.c
+++ iocbio/fperiod/src/imageinterp.c
@@ -8,8 +8,12 @@
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
-
 /*
+  void imageinterp_get_roi(image_width, image_height, image,
+                           di_size, dj_size, i0, j0, i1, j1,
+                           roi_width, roi_height, roi, roi_di_size, roi_dj_size,
+			   interpolation )
+
   imageinterp_get_roi function returns a subimage (roi) of an image
   using interpolation.
   
@@ -21,32 +25,27 @@
   image : double*
     Specify pointer to image array that uses row-storage order.
   image_di_size, image_dj_size : double
-    Specify image pixel sizes in image units.
+    Specify image pixel sizes in image units (e.g. um).
   i0, j0, i1, j1 : double
-    Specify roi center line.
-  width : double
-    Specify roi width in image units.
+    Specify roi center line in pixel units.
   roi_width, roi_height : int
     Specify the number of columns and rows of the roi image.
   interpolation : int
     Specify interpolation method: 0=nearest-neighbor interpolation,
-    1=bilinear, 2=bicubic
+    1=bilinear, 2=bicubic.
 
   Output parameters
   -----------------
 
-  roi : double *
-    Specify pointer to roi array (row-storage order)
+  roi : double*
+    Specify pointer to roi array (row-storage order) that will be
+    filled with image data.
+  roi_di_size, roi_dj_size : double*
+    Roi pixel sizes::
 
-  Notes
-  -----
-
-  If the image pixel sizes are dx and dy then the roi pixel
-  sizes will be
-    DX = L / roi_width
-    DY = width / roi_height
-  where L = hypot((i1-i0)*dx, (j1-j0)*dy)
- */
+      rdi = hypot((i1-i0)*di, (j1-j0)*dj) / hypot (i1-i0,j1-j0)
+      rdj = hypot((i1-i0)*di, (j1-j0)*dj) / hypot ((i1-i0)*dj/di,(j1-j0)*di/dj)
+*/
 
 /* http://en.wikipedia.org/wiki/Cubic_interpolation */
 inline
@@ -57,174 +56,49 @@ double CINT(double x, double pm1, double p0, double p1, double p2)
 
 void imageinterp_get_roi(int image_width, int image_height, double *image,
 			 double di_size, double dj_size,
-			 double i0, double j0, double i1, double j1, double width,
+			 double i0, double j0, double i1, double j1,
 			 int roi_width, int roi_height, double *roi,
+			 double* roi_di_size, double* roi_dj_size,
 			 int interpolation
 			 )
 {
 #define GET_IMAGE_PTR(I, J) (image + (J)*image_width + (I))
 #define GET_IMAGE(I, J) (((I)>=0 && (I)<image_width && (J)>=0 && (J)<image_height) ? (*(image + (J)*image_width + (I))) : 0.0)
 #define GET_ROI(I, J) (*(roi + (J)*roi_width + (I)))
+  
   int ri, rj;
   int ii, ji, ii1, ji1;
   double i, j;
-  double l = hypot((i1-i0)*di_size, (j1-j0)*dj_size);
-  double dt = (roi_width>1?1.0 / (roi_width-1):0.0);
-  double ds = (roi_height>1?1.0 / (roi_height-1):0.0);
-
-  double di = -(j1-j0)/l*width*0.5*dj_size/di_size;
-  double dj = +(i1-i0)/l*width*0.5*di_size/dj_size;
-  double t;
-  double s;
   double v;
-  double is, js;
   double rmi, rmj;
   double bm1, b0, b1, b2;
-
+  double r = dj_size / di_size;
   //printf("imageinterp_get_roi: i0,j0,i1,j1,rw,rh=%f,%f,%f,%f,%d,%d\n",i0,j0,i1,j1,roi_width, roi_height);
-
-  if (j0==j1 && roi_width == i1-i0+1)
+ 
+  if (j1==j0 && i1>i0 && i1-i0+1==roi_width)
     {
-      v = width*0.5/dj_size;
-      if (fabs(v-nearbyint(v))<1e-12 && nearbyint(2*v+1)==roi_height)
-	{
-	  ji = nearbyint(j0 - v);
-	  for (rj=0; rj<roi_height; ++rj, ++ji)
-	    {
-	      if (ji<0 || ji>=image_height)
-		memset(&GET_ROI(0, rj), 0, sizeof(double)*roi_width);
-	      else if (i1<image_width)
-		memcpy(&GET_ROI(0, rj), GET_IMAGE_PTR((int)i0, ji), sizeof(double)*roi_width);
-	      else
-		for (ii=i0; ii<=i1; ++ii)
-		  GET_ROI(ii-(int)i0, rj) = GET_IMAGE(ii, ji);
-	    }
-	}
-      else
-	{
-	  //printf("imageinterp_get_roi: non optimal loop, v=%e\n",v);
-	  for (rj=0; rj<roi_height; ++rj)
-	    {
-	      s = (double)rj * ds;
-	      j = (1.0-s)*(j0 - dj) + s*(j0 + dj);
-	      //j = j0 + (2*rj/(roi_height-1)-1)*width*0.5/dj_size;
-	      ji = floor(j);
-	      rmj = j-ji;
-	      ji1 = ji+1;
-	      for (ri=0; ri<roi_width; ++ri)
-		{
-		  t = (double)ri * dt;
-		  i = i0 + t*(i1-i0);
-		  ii = floor(i);
-		  rmi = i-ii;
-		  ii1 = ii+1;
-		  switch (interpolation)
-		    {
-		    case 1: /* bilinear interpolation */
-		      v = (GET_IMAGE(ii,ji)*(1.0-rmi)+GET_IMAGE(ii1,ji)*rmi)*(1.0-rmj)
-			+(GET_IMAGE(ii,ji1)*(1.0-rmi)+GET_IMAGE(ii1,ji1)*rmi)*rmj;
-		      break;
-		    case 2: /* bicubic interpolation */
-		      bm1 = CINT(rmi, GET_IMAGE(ii-1, ji-1), GET_IMAGE(ii, ji-1), GET_IMAGE(ii+1, ji-1), GET_IMAGE(ii+2, ji-1));
-		      b0 = CINT(rmi, GET_IMAGE(ii-1, ji), GET_IMAGE(ii, ji), GET_IMAGE(ii+1, ji), GET_IMAGE(ii+2, ji));
-		      b1 = CINT(rmi, GET_IMAGE(ii-1, ji+1), GET_IMAGE(ii, ji+1), GET_IMAGE(ii+1, ji+1), GET_IMAGE(ii+2, ji+1));
-		      b2 = CINT(rmi, GET_IMAGE(ii-1, ji+2), GET_IMAGE(ii, ji+2), GET_IMAGE(ii+1, ji+2), GET_IMAGE(ii+2, ji+2));
-		      v = CINT(rmj, bm1, b0, b1, b2);
-		      break;
-		    case 0:
-		    default: /* nearest-neighbor interpolation */
-		      v = GET_IMAGE(ii, ji);
-		    }
-		  GET_ROI(ri, rj) = v;
-		}
-	    }
-	}      
-    }
-  else if (j0==j1)
-    {
-      //printf("imageinterp_get_roi: non optimal loop 2\n");
+      *roi_di_size = di_size;
+      *roi_dj_size = dj_size;
       for (rj=0; rj<roi_height; ++rj)
-	{
-	  s = (double)rj * ds;
-	  j = (1.0-s)*(j0 - dj) + s*(j0 + dj);
-	  ji = floor(j);
-	  rmj = j-ji;
-	  ji1 = ji+1;
-	  if (rmj==0.0)
-	    {
-	      for (ri=0; ri<roi_width; ++ri)
-		{
-		  t = (double)ri * dt;
-		  i = i0 + t*(i1-i0);
-		  ii = floor(i);
-		  rmi = i-ii;
-		  ii1 = ii+1;
-		  if (rmi==0.0)
-		    {
-		      v = GET_IMAGE(ii, ji);
-		    }
-		  else
-		    {
-		      switch (interpolation)
-			{
-			case 1: /* bilinear interpolation */
-			  v = GET_IMAGE(ii,ji)*(1.0-rmi)+GET_IMAGE(ii1,ji)*rmi;
-			  break;
-			case 2: /* bicubic interpolation */
-			  v = CINT(rmi, GET_IMAGE(ii-1, ji), GET_IMAGE(ii, ji), GET_IMAGE(ii+1, ji), GET_IMAGE(ii+2, ji));
-			  break;
-			case 0:
-			default: /* nearest-neighbor interpolation */
-			  v = GET_IMAGE(ii, ji);
-			}
-		    }
-		  GET_ROI(ri, rj) = v;
-		}
-	    }
-	  else
-	    {
-	      for (ri=0; ri<roi_width; ++ri)
-		{
-		  t = (double)ri * dt;
-		  i = i0 + t*(i1-i0);
-		  ii = floor(i);
-		  rmi = i-ii;
-		  ii1 = ii+1;
-		  switch (interpolation)
-		    {
-		    case 1: /* bilinear interpolation */
-		      v = (GET_IMAGE(ii,ji)*(1.0-rmi)+GET_IMAGE(ii1,ji)*rmi)*(1.0-rmj)
-			+(GET_IMAGE(ii,ji1)*(1.0-rmi)+GET_IMAGE(ii1,ji1)*rmi)*rmj;
-		      break;
-		    case 2: /* bicubic interpolation */
-		      bm1 = CINT(rmi, GET_IMAGE(ii-1, ji-1), GET_IMAGE(ii, ji-1), GET_IMAGE(ii+1, ji-1), GET_IMAGE(ii+2, ji-1));
-		      b0 = CINT(rmi, GET_IMAGE(ii-1, ji), GET_IMAGE(ii, ji), GET_IMAGE(ii+1, ji), GET_IMAGE(ii+2, ji));
-		      b1 = CINT(rmi, GET_IMAGE(ii-1, ji+1), GET_IMAGE(ii, ji+1), GET_IMAGE(ii+1, ji+1), GET_IMAGE(ii+2, ji+1));
-		      b2 = CINT(rmi, GET_IMAGE(ii-1, ji+2), GET_IMAGE(ii, ji+2), GET_IMAGE(ii+1, ji+2), GET_IMAGE(ii+2, ji+2));
-		      v = CINT(rmj, bm1, b0, b1, b2);
-		      break;
-		    case 0:
-		    default: /* nearest-neighbor interpolation */
-		      v = GET_IMAGE(ii, ji);
-		    }
-		  GET_ROI(ri, rj) = v;
-		}
-	    }
-	}
+	memcpy(&GET_ROI(0, rj), GET_IMAGE_PTR((int)i0, (int)j0 + rj - (roi_height/2)), sizeof(double)*roi_width);
     }
   else
     {
-      //printf("imageinterp_get_roi: non optimal loop 3\n");
+
+      double dti = (i1-i0)/hypot ((i1-i0),(j1-j0));
+      double dtj = (j1-j0)/hypot ((i1-i0),(j1-j0));
+      double dsi = -(j1-j0)*r*r/hypot ((j1-j0)*r*r,(i1-i0));
+      double dsj = (i1-i0)/hypot ((j1-j0)*r*r,(i1-i0));
+      double l = hypot((i1-i0)*di_size, (j1-j0)*dj_size);
+      *roi_di_size = l/hypot ((i1-i0),(j1-j0));
+      *roi_dj_size = l*r/hypot ((j1-j0)*r*r,(i1-i0));
+
       for (rj=0; rj<roi_height; ++rj)
 	{
-	  s = (double)rj * ds;
-	  is = (1.0-s)*(i0 - di) + s*(i0 + di);
-	  js = (1.0-s)*(j0 - dj) + s*(j0 + dj);
 	  for (ri=0; ri<roi_width; ++ri)
 	    {
-	      t = (double)ri * dt;
-	      i = (1.0-t)*is + t*(is+i1-i0);
-	      j = (1.0-t)*js + t*(js+j1-j0);
+	      i = i0 + ri*dti + (rj-(roi_height/2))*dsi;
+	      j = j0 + ri*dtj + (rj-(roi_height/2))*dsj;
 	      ii = floor(i);
 	      ji = floor(j);
 	      rmi = i-ii;
@@ -247,9 +121,8 @@ void imageinterp_get_roi(int image_width, int image_height, double *image,
 		case 0:
 		default: /* nearest-neighbor interpolation */
 		  v = GET_IMAGE(ii, ji);
-		}
-	      
-	      GET_ROI(ri, rj) = v;
+		}	      
+	      GET_ROI(ri, rj) = v;      
 	    }
 	}
     }
@@ -257,23 +130,30 @@ void imageinterp_get_roi(int image_width, int image_height, double *image,
 
 void imageinterp_get_roi_corners(int image_width, int image_height,
 				 double di_size, double dj_size,
-				 int i0, int j0, int i1, int j1, double width,
+				 int i0, int j0, int i1, int j1, double height,
 				 double *lli, double *llj,
 				 double *lri, double *lrj,
 				 double *uri, double *urj,
 				 double *uli, double *ulj)
 {
-  double l = hypot((i1-i0)*di_size, (j1-j0)*dj_size);
-  double di = -(double)(j1-j0)/l*width*0.5*dj_size/di_size;
-  double dj = +(double)(i1-i0)/l*width*0.5*di_size/dj_size;
-  *lli = i0 - di;
-  *llj = j0 - dj;
-  *lri = i1 - di;
-  *lrj = j1 - dj;
-  *uri = i1 + di;
-  *urj = j1 + dj;
-  *uli = i0 + di;
-  *ulj = j0 + dj;
+
+  double r = dj_size / di_size;
+  double dti = (i1-i0)/hypot ((i1-i0),(j1-j0));
+  double dtj = (j1-j0)/hypot ((i1-i0),(j1-j0));
+  double dsi = -(j1-j0)*r*r/hypot ((j1-j0)*r*r,(i1-i0));
+  double dsj = (i1-i0)/hypot ((j1-j0)*r*r,(i1-i0));
+  int roi_width = (int)(hypot((i1-i0),(j1-j0))) + 1;
+  int roi_height = (int)height + 1;
+
+  *lli = i0 - (roi_height/2)*dsi;
+  *llj = j0 - (roi_height/2)*dsj;
+  *lri = i0 + roi_width*dti - (roi_height/2)*dsi;
+  *lrj = j0 + roi_width*dtj - (roi_height/2)*dsj;
+  *uri = i0 + roi_width*dti + (roi_height-(roi_height/2))*dsi;
+  *urj = j0 + roi_width*dtj + (roi_height-(roi_height/2))*dsj;
+  *uli = i0 + (roi_height-(roi_height/2))*dsi;
+  *ulj = j0 + (roi_height-(roi_height/2))*dsj;
+
 }
 
 
@@ -287,6 +167,37 @@ void imageinterp_get_roi_corners(int image_width, int image_height,
 #define PyMODINIT_FUNC void
 #endif
 
+static char py_imageinterp_get_roi_doc[] = "\
+  get_roi(image, (di, dj), (i0, i1), (j0, j1), w, interpolation) -> roi, (rdi,rdj)\n\
+  imageinterp_get_roi function returns a subimage (roi) of an image\n\
+  using interpolation.\n\
+  \n\
+  Parameters\n\
+  ----------\n\
+  image : numpy.ndarray\n\
+    Specify image array that uses row-storage order.\n\
+  image_di_size, image_dj_size : float\n\
+    Specify image pixel sizes in image units (e.g. um).\n\
+  i0, j0, i1, j1 : float\n\
+    Specify roi center line in pixel units.\n\
+  line_width : int\n\
+    Specify the width of roi center line.\n\
+  interpolation : int\n\
+    Specify interpolation method: 0=nearest-neighbor interpolation,\n\
+    1=bilinear, 2=bicubic\n\
+\n\
+  Returns\n\
+  -------\n\
+\n\
+  roi : numpy.ndarray\n\
+    Roi array (row-storage ordered) with image data. The size of roi\n\
+    is (line width, int(hypot((i1-i0),(j1-j0))) + 1).\n\
+  roi_di_size, roi_dj_size : float\n\
+    Roi pixel sizes::\n\
+\n\
+      rdi = hypot((i1-i0)*di, (j1-j0)*dj) / hypot (i1-i0,j1-j0)\n\
+      rdj = hypot((i1-i0)*di, (j1-j0)*dj) / hypot ((i1-i0)*dj/di,(j1-j0)*di/dj)\
+";
 
 static PyObject *py_imageinterp_get_roi(PyObject *self, PyObject *args)
 {
@@ -297,34 +208,28 @@ static PyObject *py_imageinterp_get_roi(PyObject *self, PyObject *args)
   double roi_di_size, roi_dj_size;
   int roi_width = 0, roi_height = 0;
   int interpolation;
-  double w, l;
+  int line_width;
   npy_intp roi_dims[] = {0, 0};
-  if (!PyArg_ParseTuple(args, "O(dd)(dd)(dd)di", &image, &di_size, &dj_size, &i0, &i1, &j0, &j1, &w, &interpolation))
+  if (!PyArg_ParseTuple(args, "O(dd)(dd)(dd)ii", &image, &di_size, &dj_size, &i0, &i1, &j0, &j1, &line_width, &interpolation))
     return NULL;
   if (!(PyArray_Check(image) && PyArray_TYPE(image) == PyArray_DOUBLE && PyArray_NDIM(image)==2))
     {
       PyErr_SetString(PyExc_TypeError,"1st argument must be rank-2 double array object");
       return NULL;
     }
-  l = hypot((i1-i0)*di_size, (j1-j0)*dj_size);
-  if (j1==j0 && 1)
-    {
-      roi_width = i1-i0+1;
-    }
+  roi_height = line_width + 1;
+  if (j1==j0)
+    roi_width = (i1>i0?i1-i0:i0-i1) + 1;
   else
-    {
-      roi_width = (int)(hypot(i1-i0, j1-j0)+1);
-    }
-  roi_height = ceil((double)roi_width * w / (l+1.0)) + 1;
-  roi_di_size = l / roi_width;
-  roi_dj_size = w / roi_height;
+    roi_width = (int)(hypot((i1-i0),(j1-j0))) + 1;
+
   roi_dims[0] = roi_height;
   roi_dims[1] = roi_width;
   roi = PyArray_SimpleNew(2, roi_dims, PyArray_DOUBLE);
   imageinterp_get_roi(PyArray_DIMS(image)[1], PyArray_DIMS(image)[0], PyArray_DATA(image),
 		      di_size, dj_size,
-		      i0, j0, i1, j1, w,
-		      roi_width, roi_height, PyArray_DATA(roi),
+		      i0, j0, i1, j1,
+		      roi_width, roi_height, PyArray_DATA(roi), &roi_di_size, &roi_dj_size,
 		      interpolation
 		      );
   return Py_BuildValue("N(dd)", roi, roi_di_size, roi_dj_size);
@@ -348,7 +253,7 @@ static PyObject *py_imageinterp_get_roi_corners(PyObject *self, PyObject *args)
 }
 
 static PyMethodDef module_methods[] = {
-  {"get_roi", py_imageinterp_get_roi, METH_VARARGS, "get_roi(image, (di, dj), (i0, i1), (j0, j1), w, interpolation) -> roi, (rdi,rdj)"},
+  {"get_roi", py_imageinterp_get_roi, METH_VARARGS, py_imageinterp_get_roi_doc},
   {"get_roi_corners", py_imageinterp_get_roi_corners, METH_VARARGS, "get_roi_corners((w,h), (di,dj), (i0, i1), (j0, j1), w) -> (ll,lr,ur,ul)"},
   {NULL}  /* Sentinel */
 };
@@ -356,10 +261,9 @@ static PyMethodDef module_methods[] = {
 PyMODINIT_FUNC
 initimageinterp(void) 
 {
-  PyObject* m = NULL;
   import_array();
   if (PyErr_Occurred())
     {PyErr_SetString(PyExc_ImportError, "can't initialize module imageinterp (failed to import numpy)"); return;}
-  m = Py_InitModule3("imageinterp", module_methods, "Provides get_roi, get_roi_corners functions.");
+  Py_InitModule3("imageinterp", module_methods, "Provides get_roi, get_roi_corners functions.");
 }
 #endif
